3a6f62edd5521a4441c0f62cf160deac338fbef4
Alifhossain2543 <alif.pab120na.12@gmail.com>
2021-08-20 11:14:41 +0600

    made few changes

---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1>Protected page started from this.</h1>
+        </div>
+    )
+}
+
+export default index

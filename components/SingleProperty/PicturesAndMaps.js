c147323bc54e0c23a8f53e77799339107d7774f7
Alif Hossain <alif.pab120na.12@gmail.com>
2021-09-27 00:58:23 +0600

    finished working on mapping.

--- components/SingleProperty/PicturesAndMaps.js
+++ components/SingleProperty/PicturesAndMaps.js
@@ -1,13 +1,13 @@
-import { Row, Upload, message, Button } from 'antd'
+import { Row, Upload, message, Button, Col, Form, Input } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { useState, useEffect } from 'react';
 import Resizer from "react-image-file-resizer";
 import axios from 'axios';
 import { useSelector } from 'react-redux'
-import ReactMapboxGl, { ZoomControl, RotationControl, Popup, Layer, ScaleControl, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { ZoomControl, RotationControl, ScaleControl, Marker } from 'react-mapbox-gl';
+import InputField from './utilsComp/InputField';
 
 import 'mapbox-gl/dist/mapbox-gl.css'; //must install mapbox-gl alongside react-mapbox-gl
-
 
 function getBase64(file) {
     return new Promise((resolve, reject) => {
@@ -35,8 +35,13 @@ const PicturesAndMaps = ({ data, geo }) => {
     const propertyId = useSelector((state) => state.property.propertyId)
     const user = useSelector((state) => state.user.user)
     const [isSatellite, setIsSatellite] = useState(false)
+    const [lat, setLat] = useState(geo.lat)
+    const [long, setLong] = useState(geo.long)
+    const [isLoading, setIsLoading] = useState(false)
 
+    const { Item } = Form
 
+    const [geoForm] = Form.useForm()
 
     const uploadButton = (
         <div>
@@ -161,10 +166,44 @@ const PicturesAndMaps = ({ data, geo }) => {
         }
     }
 
-    console.log(isSatellite)
+    const geoChangeHandler = async ({ long, lat }) => { //use useEffect for the best pratice.
+
+        try {
+            message.loading({ content: "Updating property location...", key: "7" })
+            setIsLoading(true)
+            await axios.put(`http://localhost:5000/api/update-map/${propertyId}`, { long, lat }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            message.loading({ content: "property location updated successfully", key: "7" })
+            setLong(long)
+            setLat(lat)
+            setIsLoading(false)
+
+        } catch (err) {
+            setIsLoading(false)
+            message.error({ content: err.response ? err.response.data.message : "Something went wrong.", key: "7", duration: "3.5" })
+            console.log(err)
+        }
+
+    }
 
     return (
         <Row gutter={[12, 15]} wrap={true} justify="start" style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}  >
+            <Col span={24}>
+
+                <Form form={geoForm} name="geoForm" layout="vertical" initialValues={{ long: long, lat: lat }} style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }} onFinish={geoChangeHandler} >
+
+                    <InputField name="long" label="Longitude" />
+                    <InputField name="lat" label="Latitude" />
+                    <Button type="primary" htmlType="submit" style={{ marginTop: "30px" }} loading={isLoading} disabled={isLoading} >Change Location</Button>
+
+                </Form>
+
+            </Col>
+
 
             <div style={{ border: "1px solid black", height: "300px", overflow: "auto", display: "flex", flexDirection: "row", flexWrap: "wrap", borderStyle: "dotted", padding: "10px" }}>
 
@@ -185,7 +224,7 @@ const PicturesAndMaps = ({ data, geo }) => {
                 <div style={{ height: "60vh", width: "90%", border: "1px solid black", borderStyle: "dotted", position: "relative", zIndex: "5" }}>
 
                     <div style={{ position: 'absolute', zIndex: "6" }}>
-                        <Button onClick={() => setIsSatellite(!isSatellite)} type="primary"> {isSatellite ? "Satellite View" : "Street View"}</Button>
+                        <Button onClick={() => setIsSatellite(!isSatellite)} > {isSatellite ? "Satellite View" : "Street View"}</Button>
                     </div>
                     <Map
                         style={isSatellite ? "mapbox://styles/mapbox/satellite-v9" : "mapbox://styles/mapbox/outdoors-v11"}
@@ -193,23 +232,25 @@ const PicturesAndMaps = ({ data, geo }) => {
                             height: '100%',
                             width: '100%'
                         }}
-                        center={[geo.long ? geo.long : -78.2952710, geo.lat ? geo.lat : 35.9553564]}
-                        zoom={[geo.lat || geo.long ? 15 : 0]}
+                        center={[long ? long : -78.2952710, lat ? lat : 35.9553564]}
+                        zoom={[lat || long ? 15 : 0]}
+                        renderChildrenInPortal={true}
                     >
                         <ZoomControl position="bottom-right" />
                         <RotationControl />
 
-
                         {geo && Object.keys(geo).length !== 0 &&
-                            <Popup
-                                coordinates={[geo.long, geo.lat]}
-                                offset={{
-                                    'bottom-left': [12, -38], 'bottom': [0, -38], 'bottom-right': [-12, -38]
-                                }}>
-                                <h1>Popup</h1>
-                            </Popup>
-                        }
 
+                            <Marker
+                                coordinates={[long, lat]}
+                                anchor="bottom"
+                            >
+                                <img src="/marker.png"
+                                    height={55} width={55}
+                                />
+
+                            </Marker>
+                        }
                         <ScaleControl position="bottom-left" />
 
                     </Map>;
